6bf2d4ef89eab53afeb2e4c3fac170210b072e51
Technotraxx <114928644+Technotraxx@users.noreply.github.com>
2024-06-09 15:54:49 +0200

    Update search_api.py

--- search_api.py
+++ search_api.py
@@ -1,11 +1,14 @@
-# search_api.py
-
 import requests
 from bs4 import BeautifulSoup
 
 def search_news(query):
     url = f"https://duckduckgo.com/html/?q={query}&iar=news"
     response = requests.get(url)
+    
+    # Debugging: Statuscode und Inhalt der Antwort anzeigen
+    print(f"Status Code: {response.status_code}")
+    print(f"Response Content: {response.content}")
+
     soup = BeautifulSoup(response.content, 'html.parser')
 
     links = []
