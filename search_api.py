f9a606c1eab3aac9da11c1ebf558fbd428164d44
Technotraxx <114928644+Technotraxx@users.noreply.github.com>
2024-06-09 17:12:26 +0200

    Update search_api.py

--- search_api.py
+++ search_api.py
@@ -1,6 +1,20 @@
 from duckduckgo_search import DDGS
 
-def search_news(query):
+def search_duckduckgo(query, category='news', time=None, site=None, exclude_site=None, max_results=15):
     ddgs = DDGS()
-    results = ddgs.news(keywords=f"{query} -site:msn.com", max_results=15)
-    return [result['url'] for result in results if 'msn.com' not in result['url']]
+    if exclude_site:
+        query += f" -site:{exclude_site}"
+    if site:
+        query += f" site:{site}"
+    
+    search_function = {
+        'news': ddgs.news,
+        'text': ddgs.text,
+        'images': ddgs.images,
+        'videos': ddgs.videos,
+        'maps': ddgs.maps,
+        'translate': ddgs.translate
+    }.get(category, ddgs.text)
+    
+    results = search_function(keywords=query, timelimit=time, max_results=max_results)
+    return [result['url'] for result in results]
